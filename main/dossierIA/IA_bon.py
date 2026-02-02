a48e0ea7f688906b8c13d05dbc05f23ff2e1e44e
Oli2-dab <olivier@ipsodie.com>
2026-02-02 15:00:39 -0500

    maj

--- main/dossierIA/IA_bon.py
+++ main/dossierIA/IA_bon.py
@@ -8,22 +8,22 @@
 import streamlit as st
 from IA import prédiction
 
-st.session_state.sjhumidité = st.text_input("sjhumidité")
-st.session_state.thumidité = st.text_input("thumidité")
-st.session_state.sjréchauffement = st.text_input("sjréchauffement")
-st.session_state.tréchauffement = st.text_input("tréchauffement")
-st.session_state.sjrefroidissement = st.text_input("sjrefroidissement")
-st.session_state.trefroidissement = st.text_input("trefroidissement")
+st.session_state.sjavanthumidité = st.text_input("sjavanthumidité")
+st.session_state.tavanthumidité = st.text_input("tavanthumidité")
+st.session_state.sjavantréchauffement = st.text_input("sjavantréchauffement")
+st.session_state.tavantréchauffement = st.text_input("tavantréchauffement")
+st.session_state.sjavantrefroidissement = st.text_input("sjavantrefroidissement")
+st.session_state.tavantrefroidissement = st.text_input("tavantrefroidissement")
 
 
 
 if st.button("IA") :
-    st.session_state.sjhumidité = int(st.session_state.sjhumidité)
-	st.session_state.thumidité = int(st.session_state.thumidité)
-	st.session_state.sjréchauffement = int(st.session_state.sjréchauffement)
-	st.session_state.tréchauffement = int(st.session_state.tréchauffement)
-	st.session_state.sjrefroidissement = int(st.session_state.sjrefroidissement)
-	st.session_state.trefroidissement = int(st.session_state.trefroidissement)
+    st.session_state.sjhumidité = int(st.session_state.sjavanthumidité)
+	st.session_state.thumidité = int(st.session_state.tavanthumidité)
+	st.session_state.sjréchauffement = int(st.session_state.sjavantréchauffement)
+	st.session_state.tréchauffement = int(st.session_state.tavantréchauffement)
+	st.session_state.sjrefroidissement = int(st.session_state.sjavantrefroidissement)
+	st.session_state.trefroidissement = int(st.session_state.tavantrefroidissement)
     
     résultat_IA = prédiction()
     st.success(résultat_IA)
