5a3ade4c744dc211548330d4748fa9e40e13f470
Oli2-dab <olivier@ipsodie.com>
2026-03-14 13:09:32 -0400

    maj 0.0.2

--- main/dossierIA/IA_bon.py
+++ main/dossierIA/IA_bon.py
@@ -8,12 +8,22 @@
 import streamlit as st
 from dossierIA.IA import prédiction
 
-st.session_state.sjavanthumidité = st.text_input("sjavanthumidité")
+st.session_state.sjavanthumidité = int(st.text_input("sjavanthumidité"))
 st.session_state.tavanthumidité = st.text_input("tavanthumidité")
 st.session_state.sjavantréchauffement = st.text_input("sjavantréchauffement")
 st.session_state.tavantréchauffement = st.text_input("tavantréchauffement")
 st.session_state.sjavantrefroidissement = st.text_input("sjavantrefroidissement")
 st.session_state.tavantrefroidissement = st.text_input("tavantrefroidissement")
+st.session_state.sjstabilite_air = st.text_input("sjstabilite_air")
+st.session_state.tstabilite_air = st.text_input("tstabilite_air")
+st.session_state.sjpression_atmo = st.text_input("sjpression_atmo")
+st.session_state.tpression_atmo = st.text_input("tpression_atmo")
+st.session_state.sjmasse_air = st.text_input("sjmasse_air")
+st.session_state.tmasse_air = st.text_input("tmasse_air")
+st.session_state.sjfronts = st.text_input("sjfronts")
+st.session_state.tfronts = st.text_input("tfronts")
+st.session_state.sjnuage_precipitation = st.text_input("sjnuage_precipitation")
+st.session_state.tnuage_precipitation = st.text_input("tnuage_precipitation")
 
 
 
